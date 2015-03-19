6c33a3a8272466d48dc049cae95cf337da75daf2
Aaron Gardner <aaron.gardner@yahoo.com>
2015-03-18 22:54:34 -0400

    Travel commit.

---
+++ DFL/DflBaseClass.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DFL
+{
+    public abstract class DflBaseClass
+    {
+        #region Constructors
+
+        protected DflBaseClass(string path, string encoding)
+        {
+            if (String.IsNullOrEmpty(path)) throw new ArgumentException("Null or empty path.", path);
+            if (!File.Exists(path)) throw new ArgumentException(String.Format("File {0} does not exist.", path));
+
+            var encodingObj = ValidateEncodingString(encoding);
+
+            _str = new StreamReader(path, encodingObj);
+            
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string CurrentLine { get; protected set; }
+
+        public IEnumerable<string> CurrentRecord { get; protected set; }
+
+        public IEnumerable<string> HeaderRecord { get; protected set; }
+
+        public int CurrentLineNumber { get; protected set; }
+
+        protected StreamReader _str { get; set; }
+
+        public bool EndOfFile { get; protected set; }
+
+        protected int ExpectedFieldCount { get; set; }
+
+        #endregion
+
+        #region Abstract Methods
+
+        public abstract void GetNextRecord();
+
+        #endregion
+
+        #region Methods
+
+        //Validate encoding string argument and return an Encoding object
+        protected static Encoding ValidateEncodingString(string encoding)
+        {
+            //make the string case insensitive
+            var lEncoding = encoding.ToLower(CultureInfo.InvariantCulture);
+
+            EncodingInfo encodingInfo = null;
+            try
+            {
+                encodingInfo = Encoding.GetEncodings().Single(e => e.Name.ToLowerInvariant().Contains(lEncoding));
+            }
+            catch (InvalidOperationException e)
+            {
+                throw new InvalidOperationException(String.Format("{0} is not a valid encoding", encoding), e);
+            }
+
+            return Encoding.GetEncoding(encodingInfo.Name);
+        }
+
+        protected void GetNextLine()
+        {
+            var line = _str.ReadLine();
+            if (line == null && _str.EndOfStream)
+            {
+                EndOfFile = true;
+            }
+            if (string.IsNullOrEmpty(line)) throw new ApplicationException("Empty line.");
+            CurrentLine = line;
+            CurrentLineNumber++;
+        }
+
+        public static IEnumerable<string> ParseMultiValueField(string fieldValue, char multiValueDelimiter, bool omitEmptyValues, bool trimWhitespace)
+        {
+            if (String.IsNullOrEmpty(fieldValue)) return new List<string>();
+            var mvdArray = new[] { multiValueDelimiter };
+            var sso = omitEmptyValues ? StringSplitOptions.RemoveEmptyEntries : StringSplitOptions.None;
+            var splitResult = fieldValue.Split(mvdArray, sso);
+            return trimWhitespace ? splitResult.Select(v => v.Trim()).ToList() : splitResult.ToList();
+        }
+
+        #endregion
+    }
+}
---
+++ FixedWidthFileLibrary/FixedWidthFile.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DFL;
+
+namespace FixedWidthFileLibrary
+{
+    public class FixedWidthFile : DflBaseClass
+    {
+        #region Constructors
+        
+        public FixedWidthFile(string path, string encoding)
+            : base(path, encoding)
+        { }
+        
+        public FixedWidthFile(string path, string encoding, List<int> startPositions)
+            : base(path, encoding)
+        {
+            for (var i = 0; i < startPositions.Count; i++)
+            {
+                Fields.Add(new Field(i.ToString(CultureInfo.InvariantCulture), startPositions[i]));
+            }
+            FieldStartPositionsKnown = true;
+        }
+
+        #endregion
+
+        #region Public Properties and Methods
+
+        public bool FieldStartPositionsKnown { get; private set; }
+
+        public void GuessFieldStartPositions()
+        {
+            GetNextLine();
+            var line = CurrentLine;
+            for (var i = 0; i < line.Length; i++)
+            {
+                var character = line[i];
+                
+            }
+        }
+
+        public override void GetNextRecord()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Properties and Methods
+
+        private List<Field> Fields { get; set; }
+
+        #endregion
+    }
+
+    public class Field
+    {
+        #region Constructors
+
+        public Field(string fieldName, int startPosition)
+        {
+            if (string.IsNullOrEmpty(fieldName)) throw new ArgumentException("Invalid field name.", "fieldName");
+            if (startPosition < 0)
+                throw new ArgumentException("Field start position cannot be less than zero.", "startPosition");
+            FieldName = fieldName;
+            StartPosition = startPosition;
+        }
+
+        #endregion
+
+        #region Fields
+
+        private int _endPosition;
+
+        #endregion
+
+        #region Properties
+
+        public string FieldName { get; private set; }
+
+        public int StartPosition { get; private set; }
+
+        public int EndPosition
+        {
+            get { return _endPosition; }
+            set
+            {
+                if (value < 0) throw new Exception("End position cannot be negative.");
+                if (value < StartPosition) throw new Exception("End position cannot be less than start position.");
+                _endPosition = value;
+            }
+        }
+
+        #endregion
+    }
+}
